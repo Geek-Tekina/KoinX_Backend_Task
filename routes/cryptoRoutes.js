04fe736934c7f09ca17ca0df65c06cf2301d75da
Geek-Tekina <tpbc1717@gmail.com>
2025-01-10 20:08:40 +0530

    added standard deviation calculator and created /deviation route

--- routes/cryptoRoutes.js
+++ routes/cryptoRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Crypto = require("../models/crypto");
+const { calculateStandardDeviation } = require("../utils/mathUtils");
 const router = express.Router();
 
 router.get("/stats", async (req, res) => {
@@ -15,6 +16,27 @@ router.get("/stats", async (req, res) => {
       marketCap: latestData.marketCap,
       "24hChange": latestData.change24h,
     });
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+router.get("/deviation", async (req, res) => {
+  const { coin } = req.query;
+
+  try {
+    const data = await Crypto.find({ coin })
+      .sort({ createdAt: -1 })
+      .limit(100)
+      .select("price");
+    // console.log(data);
+    if (!data.length)
+      return res.status(404).json({ error: "No data found for this coin" });
+
+    const prices = data.map((record) => record.price);
+    const deviation = calculateStandardDeviation(prices);
+
+    res.json({ deviation });
   } catch (error) {
     res.status(500).json({ error: "Server error" });
   }
